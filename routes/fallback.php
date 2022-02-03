c5ad9cf41740a966b23da906ac63ab991ad0ccee
Robin Mulder <robin@justbetter.nl>
2022-02-03 15:46:28 +0100

    merge conflict

--- routes/fallback.php
+++ routes/fallback.php
@@ -14,10 +14,7 @@ if ($brand = DB::table('amasty_amshopby_option_setting')
     $brand->name = $option->value;
 
     echo view('amastyshopbybrand::brand-overview', compact('brand'));
-<<<<<<< HEAD
 
-=======
->>>>>>> 0aad2c1d6b1a5d0b35d4f603f8c94ece80e402d7
 } elseif ($option = DB::table('eav_attribute_option')
     ->join('eav_attribute', 'eav_attribute.attribute_id', '=', 'eav_attribute_option.attribute_id')
     ->where('eav_attribute.attribute_code', Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'))
