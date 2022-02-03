a28f385aaafca11ced696408d1ec78dd1101a0ae
Robin Mulder <robin@justbetter.nl>
2022-02-03 16:51:09 +0100

    StyleCIBot changes

--- routes/fallback.php
+++ routes/fallback.php
@@ -14,7 +14,6 @@ if ($brand = DB::table('amasty_amshopby_option_setting')
     $brand->name = $option->value;
 
     echo view('amastyshopbybrand::brand-overview', compact('brand'));
-
 } elseif ($option = DB::table('eav_attribute_option')
     ->join('eav_attribute', 'eav_attribute.attribute_id', '=', 'eav_attribute_option.attribute_id')
     ->where('eav_attribute.attribute_code', Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'))
@@ -26,7 +25,7 @@ if ($brand = DB::table('amasty_amshopby_option_setting')
     })
     ->first()) {
     if ($brand = DB::table('amasty_amshopby_option_setting')
-        ->where('value',$option->option_id)
+        ->where('value', $option->option_id)
         ->where('store_id', 0)
         ->first()) {
         $brand->name = $option->value;
