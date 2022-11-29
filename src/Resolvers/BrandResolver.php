cc46e8dc8d6a005519d19802e00bcfab25a23657
Claudio Ferraro <claudio@justbetter.nl>
2022-11-29 14:07:16 +0100

    Fix styleci

--- src/Resolvers/BrandResolver.php
+++ src/Resolvers/BrandResolver.php
@@ -2,8 +2,8 @@
 
 namespace Rapidez\AmastyShopByBrand\Resolvers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Rapidez\Core\Facades\Rapidez;
 
 class BrandResolver
