1a0bf716fbff844973091a68d6ba62ae90b9d8f8
Claudio Ferraro <claudio@justbetter.nl>
2022-11-29 13:44:14 +0100

    Get brand data from store view with fallback on default store

--- routes/fallback.php
+++ routes/fallback.php
@@ -1,18 +1,28 @@
 <?php
 
-if ($brand = DB::table('amasty_amshopby_option_setting')
-    ->select('*')
+$rawSelect = '';
+foreach (Schema::getColumnListing('amasty_amshopby_option_setting') ?? [] as $column) {
+    $rawSelect .= "COALESCE(NULLIF(store_option.$column, ''), main_option.$column) as $column,";
+}
+$rawSelect = rtrim($rawSelect, ',');
+
+if ($rawSelect && $brand = DB::table('amasty_amshopby_option_setting AS main_option')
+    ->selectRaw($rawSelect)
     ->selectSub(
         DB::table('catalog_product_flat_'.config('rapidez.store'))
             ->selectRaw('COUNT(*)')
-            ->whereColumn(Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'), 'value'),
+            ->whereColumn(Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'), 'main_option.value'),
         'product_count'
     )
-    ->where(function ($query) {
-        $query->where('url_alias', '/'.request()->path())
-            ->orWhere('url_alias', request()->path());
+    ->leftJoin('amasty_amshopby_option_setting AS store_option', function ($join) {
+        $join->on('store_option.value', 'main_option.value')
+            ->where('store_option.store_id', config('rapidez.store'));
     })
-    ->where('store_id', 0)
+    ->where(function ($query) {
+        $query->where('main_option.url_alias', '/'.request()->path())
+            ->orWhere('main_option.url_alias', request()->path());
+    })
+    ->where('main_option.store_id', 0)
     ->first()) {
     $option = DB::table('eav_attribute_option_value')
         ->where('option_id', $brand->value)
@@ -25,23 +35,27 @@ if ($brand = DB::table('amasty_amshopby_option_setting')
     ->join('eav_attribute', 'eav_attribute.attribute_id', '=', 'eav_attribute_option.attribute_id')
     ->where('eav_attribute.attribute_code', Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'))
     ->join('eav_attribute_option_value', 'eav_attribute_option_value.option_id', '=', 'eav_attribute_option.option_id')
-    ->where('store_id', 0)
+    ->where('eav_attribute_option_value.store_id', config('rapidez.store'))
     ->where(function ($query) {
         $query->where('value', str_replace('_', ' ', request()->path()))
               ->orWhere('value', str_replace('-', ' ', request()->path()))
               ->orWhere('value', str_replace('_', '-', request()->path()));
     })
     ->first()) {
-    if ($brand = DB::table('amasty_amshopby_option_setting')
-        ->select('*')
+    if ($rawSelect && $brand = DB::table('amasty_amshopby_option_setting AS main_option')
+        ->selectRaw($rawSelect)
         ->selectSub(
             DB::table('catalog_product_flat_'.config('rapidez.store'))
                 ->selectRaw('COUNT(*)')
                 ->where($option->attribute_code, $option->option_id),
             'product_count'
         )
-        ->where('value', $option->option_id)
-        ->where('store_id', 0)
+        ->leftJoin('amasty_amshopby_option_setting AS store_option', function ($join) {
+            $join->on('store_option.value', 'main_option.value')
+            ->where('store_option.store_id', config('rapidez.store'));
+        })
+        ->where('main_option.value', $option->option_id)
+        ->where('main_option.store_id', 0)
         ->first()) {
         $brand->name = $option->value;
 
