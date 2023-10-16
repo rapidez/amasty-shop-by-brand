c4a88d633be77ea93c755231cda4f8085cc791a7
indy koning <indy@justbetter.nl>
2023-10-16 09:03:57 +0200

    Improve codestyle

--- src/Models/AmastyAmshopbyOptionSetting.php
+++ src/Models/AmastyAmshopbyOptionSetting.php
@@ -11,22 +11,17 @@ class AmastyAmshopbyOptionSetting extends Model
 
     protected $primaryKey = 'option_setting_id';
 
-    protected static function booting()
-    {
-        // static::addGlobalScope(fn($query) => $query
-        //     ->leftJoin()
-        // );
-    }
-
     public function getNameAttribute()
     {
         $this->loadMissing(['optionValue']);
+
         return $this->optionValue->value;
     }
 
     public function fillMissing()
     {
         $this->loadMissing(['defaultAmshopbyOptionSetting']);
+
         foreach (array_filter($this->getAttributes(), fn ($val) => empty($val) && $val !== 0) as $attribute => $value) {
             $this->$attribute = $this->defaultAmshopbyOptionSetting?->$attribute;
         }
@@ -44,7 +39,8 @@ class AmastyAmshopbyOptionSetting extends Model
         ->where('store_id', 0);
     }
 
-    public function products() {
+    public function products() 
+    {
         return $this->hasMany(
             config('rapidez.models.product'),
             Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'),
@@ -52,7 +48,8 @@ class AmastyAmshopbyOptionSetting extends Model
         )->withoutGlobalScopes();
     }
 
-    public function optionValue() {
+    public function optionValue() 
+    {
         return $this->hasOne(
             config('rapidez.models.optionvalue'),
             'option_id',
--- src/Resolvers/BrandResolver.php
+++ src/Resolvers/BrandResolver.php
@@ -13,22 +13,28 @@ class BrandResolver
     {
         $path = $path ?: request()->path();
 
-        $brand = AmastyAmshopbyOptionSetting::with(['optionValue', 'defaultAmshopbyOptionSetting'])
+        return AmastyAmshopbyOptionSetting::with(['optionValue', 'defaultAmshopbyOptionSetting'])
             ->withCount('products')
-            ->where(fn ($query) => $query
+            ->where(
+                fn ($query) => $query
                 ->where('store_id', config('rapidez.store'))
-                ->where(fn ($query) => $query
+                ->where(
+                    fn ($query) => $query
                     ->where('url_alias', '/' . $path)
                     ->orWhere('url_alias', $path)
-                    ->orWhereHas('defaultAmshopbyOptionSetting', fn ($query) => $query
+                    ->orWhereHas(
+                        'defaultAmshopbyOptionSetting', 
+                        fn ($query) => $query
                         ->where('url_alias', '/' . $path)
                         ->orWhere('url_alias', $path)
                     )
                 )
             )
-            ->orWhere(fn ($query) => $query
+            ->orWhere(
+                fn ($query) => $query
                 ->where('store_id', 0)
-                ->where(fn ($query) => $query
+                ->where(
+                    fn ($query) => $query
                     ->where('url_alias', '/' . $path)
                     ->orWhere('url_alias', $path)
                 )
@@ -36,15 +42,13 @@ class BrandResolver
             ->orderByDesc('store_id')
             ->first()
             ?->fillMissing();
-
-        return $brand;
     }
 
     public function byOptionValue(string $path = ''): ?object
     {
         $path = $path ?: request()->path();
 
-        $attributes = config('rapidez.models.attribute')::getCachedWhere(fn($attribute) => $attribute['code'] === Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'));
+        $attributes = config('rapidez.models.attribute')::getCachedWhere(fn ($attribute) => $attribute['code'] === Rapidez::config('amshopby_brand/general/attribute_code', 'manufacturer'));
         if (!count($attributes)) {
             return null;
         }
@@ -52,10 +56,14 @@ class BrandResolver
 
         return AmastyAmshopbyOptionSetting::with(['optionValue', 'defaultAmshopbyOptionSetting'])
         ->withCount('products')
-        ->where(fn ($query) => $query
+        ->where(
+            fn ($query) => $query
             ->whereIn('store_id', [0, config('rapidez.store')])
-            ->whereHas('optionValue', fn($query) => $query
-                ->where(fn ($query) => $query
+            ->whereHas(
+                'optionValue', 
+                fn($query) => $query
+                ->where(
+                    fn ($query) => $query
                     ->where('value', str_replace('_', ' ', $path))
                     ->orWhere('value', str_replace('-', ' ', $path))
                     ->orWhere('value', str_replace('_', '-', $path))
@@ -71,15 +79,5 @@ class BrandResolver
     public static function bind(): void
     {
         app()->singleton(BrandResolver::class, static::class);
-    }
-
-    public function getRawSelect(): string
-    {
-        $rawSelect = '';
-        foreach (Schema::getColumnListing('amasty_amshopby_option_setting') ?? [] as $column) {
-            $rawSelect .= "COALESCE(NULLIF(store_option.$column, ''), main_option.$column) as $column,";
-        }
-
-        return rtrim($rawSelect, ',');
     }
 }
